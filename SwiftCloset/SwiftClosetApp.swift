facfc0306a21ad64e73ab5a456a3ba5a18340f3b
SunjaeJadenLee <sj.jaden1118@gmail.com>
2021-03-10 18:53:48 +0900

    chore: init project with swift code

---
+++ SwiftCloset/SwiftClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClosetApp.swift
+//  SwiftCloset
+//
+//  Created by Sunjae's Mac on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
